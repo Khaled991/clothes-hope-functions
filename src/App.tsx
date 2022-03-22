273e2a0e6fc1dce0595def79cbe4c78801f0ecf6
ahmed9190 <hamadawaled11@gmail.com>
2022-03-23 00:15:18 +0200

    merge ahmed and master

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-import { useAppSelector, useAppDispatch } from './redux/hooks';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  selectCount,
-} from './redux/counter/counter.slice';
+// import { useAppSelector, useAppDispatch } from "./redux/hooks";
+import ProductCard from "./components/product-card";
+
 function App() {
-  const count = useAppSelector(selectCount);
-  const dispatch = useAppDispatch();
+  // const count = useAppSelector(selectCount);
+  // const dispatch = useAppDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>{count}</h1>
-
-        <button onClick={() => dispatch(decrement())}>-</button>
-        <button onClick={() => dispatch(increment())}>+</button>
-        <button onClick={() => dispatch(incrementByAmount(5))}>
-          increment by 5
-        </button>
-      </header>
+      <ProductCard
+        id={1}
+        name="Hello"
+        imageUrl="https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg"
+        rate={2}
+        price={100}
+      />
     </div>
   );
 }
