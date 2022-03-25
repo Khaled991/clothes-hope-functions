18d924a79e29eea6e734345a745d0b2074b367f2
Khaled991 <khaled.waleed5357@gmail.com>
2022-03-25 12:05:47 +0200

    remove commented redux

--- src/redux/cart/cart.selector.ts
+++
@@ -1,7 +0,0 @@
-// import type { RootState } from "../store";
-
-// // Other code such as selectors can use the imported `RootState` type
-// export const selectCart = (state: RootState) => state.cart;
-// export const selectHiddenCart = (state: RootState) => state.cart.hidden;
-// export const selectCartItems = (state: RootState) => state.cart.cartItems;
-export {};
